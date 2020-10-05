3ddffebabf91bf9d4d9a2dbcb75a736cc2dbd0c8
albertmensah <DMensah@madisoncollege.edu>
2020-10-04 21:13:11 -0500

    Trying to debug the issue

--- src/App.js
+++ src/App.js
@@ -8,40 +8,40 @@ import { API } from 'aws-amplify';
 
 const App = () => {
 
-// Create coins variable and set to empty array
-const [coins, updateCoins] = useState([]);
+    // Create coins variable and set to empty array
+    const [coins, updateCoins] = useState([]);
 
-// Define function to all API
-const fetchCoins = async () => {
-    const data = await API.get('cryptoapi', '/coins');
-    updateCoins(data.coins);
+    // Define function to all API
+    const fetchCoins = async () => {
+        const data = await API.get('cryptoapi', '/coins');
+        updateCoins(data.coins);
 
- }
+    }
 
-// Call fetchCoins function when component loads
-useEffect(
-() => {
-  fetchCoins();
-   }
-, []
-);
+    // Call fetchCoins function when component loads
+    useEffect(
+        () => {
+        fetchCoins();
+        }
+        , []
+    );
 
-  return (
-   <div className="App">
-       {
-        coins.map(x => (
-          <div >
-            <h2>
-              { x.name } - { x.symbol }
-            </h2>
-            <h5>
-              ${ x.price_usd }
-            </h5>
-          </div>
-         ))
-       }
-     </div>
-  );
+    return (
+        <div className="App">
+        {
+            coins.map(x => (
+            <div >
+                <h2>
+                    { x.name } - { x.symbol }
+                </h2>
+                <h5>
+                    ${ x.price_usd }
+                </h5>
+            </div>
+            ))
+        }
+        </div>
+    );
 }
 
 export default App;
