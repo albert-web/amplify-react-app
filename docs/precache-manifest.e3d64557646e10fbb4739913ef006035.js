174be67512da6ae921434ba42eb52ef0c0e4bfcf
albertmensah <DMensah@madisoncollege.edu>
2020-10-07 13:04:59 -0500

    Attempting GitHub publish pages

---
+++ docs/precache-manifest.e3d64557646e10fbb4739913ef006035.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "240204b15f0bf87cdd83b1e8aad54169",
+    "url": "/amplify-react-app/index.html"
+  },
+  {
+    "revision": "db5f288c88161cae9733",
+    "url": "/amplify-react-app/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "640594636de2ce982708",
+    "url": "/amplify-react-app/static/js/2.058d3324.chunk.js"
+  },
+  {
+    "revision": "3d4d9872e604682782346535cc277e64",
+    "url": "/amplify-react-app/static/js/2.058d3324.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "db5f288c88161cae9733",
+    "url": "/amplify-react-app/static/js/main.9f0ba9e5.chunk.js"
+  },
+  {
+    "revision": "47435f97aff3063d93de",
+    "url": "/amplify-react-app/static/js/runtime-main.697215da.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/amplify-react-app/static/media/logo.5d5d9eef.svg"
+  }
+]);
